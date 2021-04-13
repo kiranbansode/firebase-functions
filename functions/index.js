71f4b51b0fa532008f6239ce7b56e7b094afe41e
kiranbansode <kiran5120135@gmail.com>
2021-04-13 15:55:39 +0530

    added functions in app and deployed them to firebase

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// http function 1
+exports.randomNumber = functions
+    .region('asia-south1')
+    .https.onRequest((request, response) => {
+        const number = Math.round(Math.random() * 100);
+        response.send(number.toString());
+    });
+
+// http function 2
+exports.toGoogle = functions
+    .region('asia-south1')
+    .https.onRequest((request, response) => {
+        response.redirect('https://www.google.com');
+    });
